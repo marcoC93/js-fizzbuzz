4e6084038e925508506721dbe652a923c7697aa1
Marco <marco.chianese93@gmail.com>
2020-10-20 15:38:19 +0200

    stampa multipli di 3 e 5 "fizzbuzz"

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ var fizz = "fizz" ;
 var buzz = "buzz" ;
 var fizzbuzz = "fizzbuzz" ;
 
+//iniziamo il ciclo for che si ripeterá per 100 volte
 for (var i = 0; i < 100; i++) {
     array.push(i)
-    if (array[i] % 3 == 0 )   {
-        console.log(fizz);
+    if (array[i] % 3 == 0 && array[i] % 5 == 0 )   {
+        console.log(fizzbuzz);
     } else if (array[i] % 5 == 0) {
         console.log(buzz);
+    } else if (array[i] % 3 == 0){
+        console.log(fizz);
     } else {
         console.log(i);
     }
