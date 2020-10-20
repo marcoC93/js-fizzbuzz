dd2ded4ca35157cb2df5bd72da007a687a599ade
Marco <marco.chianese93@gmail.com>
2020-10-20 15:30:57 +0200

    stampa numeri multipli di 5 "buzz"

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ for (var i = 0; i < 100; i++) {
     array.push(i)
     if (array[i] % 3 == 0 )   {
         console.log(fizz);
+    } else if (array[i] % 5 == 0) {
+        console.log(buzz);
     } else {
         console.log(i);
     }
